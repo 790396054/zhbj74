d7889ff64ef2be89e8dd266d07b9b2f44fce3ecd
gmm <miaomiaogong92@gmail.com>
2016-08-03 11:02:19 +0800

    添加 slidingMenu 库文件

--- app/src/main/java/itheima/com/zhbj74/MainActivity.java
+++ app/src/main/java/itheima/com/zhbj74/MainActivity.java
@@ -1,16 +1,25 @@
 package itheima.com.zhbj74;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
+import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
 
 /**
  * 首页
  */
-public class MainActivity extends Activity {
+public class MainActivity extends SlidingFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);//去掉标题
         setContentView(R.layout.activity_main);
+
+        setBehindContentView(R.layout.left_menu);
+        SlidingMenu slidingMenu = getSlidingMenu();
+        slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        slidingMenu.setBehindOffset(200);
     }
 }
